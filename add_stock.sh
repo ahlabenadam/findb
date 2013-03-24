42b22176b83dd9fd951d83c8ab6754cabd0b12c2
Al <ahla.benadam@gmail.com>
2013-03-24 21:58:09 +0200

    Create whole DB

--- add_stock.sh
+++ add_stock.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-DB=test
+
+DB=test.db
+LIST="st_list"
+
+rm $DB
 SQLITE="sqlite3 $DB"
 SYMBOL=$1
 
-$SQLITE 'CREATE TABLE if NOT EXISTS stock(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL UNIQUE);'
+$SQLITE 'CREATE TABLE if NOT EXISTS st_list(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL UNIQUE);'
 $SQLITE 'CREATE TABLE if NOT EXISTS watch(id INTEGER, watch INTEGER NOT NULL);'
-CMD="INSERT INTO stock(id, name) values(NULL, \"$1\");"
- $SQLITE "$CMD"
-if [ $? -ne 0 ];then
-	echo "Unable to add stock to the list"
-	exit 1
-fi
+
+while read line;do
+	CMD="INSERT INTO st_list(id, name) values(NULL, \"$line\");"
+	 $SQLITE "$CMD"
+	if [ $? -ne 0 ];then
+		echo "Unable to add stock to the list"
+		exit 1
+	fi
+
+	#sopen, last, high, low, volume, avgvol, mktcap, stdate, cdate, ctime)  values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+	CMD="CREATE TABLE if NOT EXISTS \"$line\"(sopen INTEGER, last INTEGER, high INTEGER, low INTEGER, volume INTEGER, \
+		avgvol INTEGER, mktcap INTEGER, stdate TEXT, cdate TEXT, ctime TEXT)"
+	$SQLITE "$CMD"
+done < <(cat $LIST)
+
 #$SQLITE "INSERT INTO watch(id,watch) values(
+$SQLITE 'select * from st_list'
